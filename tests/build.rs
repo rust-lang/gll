LykenSol-abandoned/lyken

gll: split off tests into gll_tests.

gll_tests/build.rs
@@ -1,0 +1,22 @@
+#![feature(decl_macro)]
+
+extern crate lyken_gll;
+
+use lyken_gll::grammar::grammar;
+use std::env;
+use std::fs::File;
+use std::path::PathBuf;
+
+fn main() {
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+
+    let mut gamma0 = grammar!{
+        Gamma0;
+        A = X { 'a' A 'b' }
+          | Y { 'a' A 'c' }
+          | Z { 'a' };
+    };
+    gamma0
+        .generate(&mut File::create(&out_dir.join("gamma0.rs")).unwrap())
+        .unwrap();
+}

rust-lang/gll

tests/build.rs
@@ -1,0 +1,22 @@
+#![feature(decl_macro)]
+
+extern crate gll;
+
+use gll::grammar::grammar;
+use std::env;
+use std::fs::File;
+use std::path::PathBuf;
+
+fn main() {
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+
+    let mut gamma0 = grammar!{
+        Gamma0;
+        A = X { 'a' A 'b' }
+          | Y { 'a' A 'c' }
+          | Z { 'a' };
+    };
+    gamma0
+        .generate(&mut File::create(&out_dir.join("gamma0.rs")).unwrap())
+        .unwrap();
+}